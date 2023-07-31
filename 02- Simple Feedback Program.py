7ecd7ae92937109be7fb06b47dbb971237a940f4
James-chuks <109963528+James-chuks@users.noreply.github.com>
2023-07-31 09:23:31 +0100

    Update 02- Simple Feedback Program.py

--- 02- Simple Feedback Program.py
+++ 02- Simple Feedback Program.py
@@ -8,41 +8,41 @@
 # So we will start by:
 # 01- Creating a variable name, which will hold some question detail, and whatever we type afterward will be stored in the variable.
 
-name = input ("Please, tell me your name (User): ") 
+#name = input ("Please, tell me your name (User): ") 
 
 # And we can print the input of name as a variable that is print (name), but then: we can further add 'hello' to the output
 # meaning we will concatenate two words into the string output.
 # ~ meaning hello + name so that the output will look different such as:
 
-print ('hello ' + name)       # When we run this we will have the output as hello and the name that will be supplied,
+# print ('hello ' + name)       # When we run this we will have the output as hello and the name that will be supplied,
                         # If you don't give space after o before the quote i.e.  'hello '  then your result will be jam-packed.
 
 # 02- We can also ask our audience to tell us their age, we will add another input as this:
-age = input('...what is your age: ')
+# age = input('...what is your age: ')
 
-print ('Okay ' + age)
+# print ('Okay ' + age)
 
 # 03- Maybe you want to let them you may need other things from them,
 
 ""
-print ('I will love to know more about you, are you willing to proceed? ')
+# print ('I will love to know more about you, are you willing to proceed? ')
 ""
 
 # 04- we can also ask them another question if, for instance, we want to find out about their complexion
-complexion = input ('...what is your complexion, (black or white): ')
+# complexion = input ('...what is your complexion, (black or white): ')
 
-print ('cool, ' + complexion)
+# print ('cool, ' + complexion)
 
 
 # 05- We can go further to ask them more questions if want, maybe their favorite food 
-food = input ('...what is your favorite food, (Garri & Soup or Rice): ')
+# food = input ('...what is your favorite food, (Garri & Soup or Rice): ')
 
-print ('wow, same as me too ' + food)
+# print ('wow, same as me too ' + food)
 
 # So you can see that for every input we customized the output result plus what the user supplied.
 
 # 06- So to close out of our Simple Feedback program, we can say:
 
-print ('Alright Bye, See you again!. ')
+# print ('Alright Bye, See you again!. ')
 
 
